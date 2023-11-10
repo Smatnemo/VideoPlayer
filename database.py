889df8b66d757073ff1887856ec6e1a2ed513ea0
Tertese Moses <Smatnemo25@gmail.com>
2023-11-10 18:52:30 +0100

    Created general database template

---
+++ database.py
@@ -0,0 +1,26 @@
+import sqlite3 
+
+# Write queries to run and create tables in database
+
+conn = sqlite3.connect("/home/tertese/")
+class DataBase(object):
+    def __init__(self):
+        pass 
+
+    def __insert__(self):
+        pass 
+
+    def __delete__(self):
+        pass 
+
+    def __update__(self):
+        pass 
+
+    def __find__(self):
+        pass 
+
+    def open(self):
+        pass 
+
+    def close(self):
+        pass
